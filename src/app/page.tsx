1762c7a9ac5466da6abc57f75859041d578298a2
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-01 17:13:19 +0700

    Home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,44 @@ import {
   RegisterLink,
   LoginLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const {isAuthenticated} = getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold text-center">
-        Unlock Your Potential with Skorona
-      </h1>
-      <div className="flex gap-4 mt-6">
-        <Button >
-          <RegisterLink>Get Started</RegisterLink>
-        </Button>
-        <Button variant={"outline"} >
-          <LoginLink>Sign In</LoginLink>
-        </Button>
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight max-w-4xl mx-auto">
+            Unlock the power of your tasks and projects with{" "}
+            <span className="text-amber-700 font-bold">Skorona</span>
+          </h1>
+          <p className="mt-6 text-base lg:text-lg text-gray-600 max-w-2xl mx-auto">
+            Stay organized, meet deadlines, and supercharge your productivity
+            with Skorona's intuitive task management
+          </p>
+
+          <div className="flex items-center justify-center gap-4 mt-6">
+            {isLoggedIn ? (
+              <>
+              <Button> 
+                <Link href="/workspace">Workspace</Link>
+              </Button>
+              </>
+            ) : (
+              <>
+                <Button >
+                  <RegisterLink>Get Started</RegisterLink>
+                </Button>
+                <Button variant={"outline"}>
+                  <LoginLink>Sign In</LoginLink>
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
