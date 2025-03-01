7d8febed7ade00d7d0b738ad15a57b052794345b
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-01 17:39:56 +0700

    Auth and Workspace Action created

---
+++ src/action/user/user-auth.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export const userRequired = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  const user = await getUser();
+
+  if (!isUserAuthenticated) redirect("/sign-in");
+
+  return { user, isUserAuthenticated };
+};
---
+++ src/action/workspace/get-user-workspaces.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { userRequired } from "../user/user-auth";
+import { db } from "../../lib/db/prisma";
+
+export const getUserWorkspaces = async () => {
+  try {
+    // Get the user from the session
+    const { user } = await userRequired();
+
+    // Get the workspaces for the user
+    const workspaces = await db.user.findUnique({
+      where: { id: user.id },
+      include: {
+        // Include the workspaces for the user
+        workspaces: {
+          select: {
+            userId: true,
+            workspaceId: true,
+            accessLevel: true,
+            createdAt: true,
+            workspace: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return {data: workspaces};
+  } catch (error) {
+    console.log(error);
+    return{
+      success: false,
+      error: true,
+      message: "Failed to get user workspaces",
+      status: 500,
+    }
+  }
+};
