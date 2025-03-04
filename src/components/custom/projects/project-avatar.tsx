fec7035dabdcd589ba850efd3282b0c5051f38d1
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-04 14:46:48 +0700

    style(project-avatar): update style on avatar component

--- src/components/custom/projects/project-avatar.tsx
+++ src/components/custom/projects/project-avatar.tsx
@@ -1,10 +1,21 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
-type Props = {}
 
-const ProjectAvatar = (props: Props) => {
+const ProjectAvatar = ({
+name,
+className
+}: {
+name: string
+className?: string
+}) => {
   return (
-    <div>ProjectAvatar</div>
+    <Avatar className={cn("size-6 2xl:size-8 rounded-md items-center", className)}>
+        <AvatarFallback className='w-6 2xl:w-8 h-6 2xl:h-8 bg-amber-600 text-base text-white rounded-md'>
+            {name.charAt(0).toUpperCase()}
+        </AvatarFallback>
+    </Avatar>
   )
 }
 
