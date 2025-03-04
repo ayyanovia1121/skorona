7f411626bf40d4d3080942b875ac32ab8f8d4176
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-04 14:48:59 +0700

    fix(project-header): fix Project Avatar Component properties

--- src/components/custom/projects/project-header.tsx
+++ src/components/custom/projects/project-header.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { ProjectProps } from '@/types'
+import ProjectAvatar from './project-avatar'
 
-type Props = {}
 
-const ProjectHeader = (props: Props) => {
+const ProjectHeader = (project:ProjectProps) => {
   return (
-    <div>ProjectHeader</div>
+    <div className='space-y-4'>
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-2">
+            <div className="flex gap-2">
+                <ProjectAvatar  name={project.name}/>
+            </div>
+        </div>
+    </div>
   )
 }
 
