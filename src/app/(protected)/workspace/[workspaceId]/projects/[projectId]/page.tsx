b370a3adb890af324ebbc1c90dc4e2ab498868cd
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-04 13:29:13 +0700

    feat(project): add tabs for Dashboard, Table and Kanban features

--- src/app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,14 +1,51 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 import React from "react";
 
 interface ProjectPageProps {
   params: Promise<{ workspaceId: string; projectId: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
-const ProjectPage = async(props: ProjectPageProps) => {
-    const { workspaceId, projectId } = await props.params;
-    const searchParams = await props.searchParams;
-    
-  return <div>ProjectByIdPage</div>;
+const ProjectPage = async (props: ProjectPageProps) => {
+  const { workspaceId, projectId } = await props.params;
+  const searchParams = await props.searchParams;
+
+  return (
+    <div className="flex flex-col gap-6 pb-3">
+      <Tabs
+        defaultValue={(searchParams.view as string) || "dashboard"}
+        className="w-full"
+      >
+        <TabsList className="mb-4">
+          <Link href="?view=dashboard">
+            <TabsTrigger value="dashboard" className="px-1.5 md:px-3">
+              Dashboard
+            </TabsTrigger>
+          </Link>
+          <Link href="?view=table">
+            <TabsTrigger value="table" className="px-1.5 md:px-3">
+              Table
+            </TabsTrigger>
+          </Link>
+          <Link href="?view=kanban">
+            <TabsTrigger value="kanban" className="px-1.5 md:px-3">
+              Kanban
+            </TabsTrigger>
+          </Link>
+        </TabsList>
+
+        <TabsContent value="dashboard">
+          <p>Dashboard</p>
+        </TabsContent>
+        <TabsContent value="table">
+          <p>Table</p>
+        </TabsContent>
+        <TabsContent value="kanban">
+          <p>Kanban</p>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ProjectPage;
