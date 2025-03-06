65bd3efd6ec052977fe02fa38dd21d1f499a6a1c
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-06 13:44:50 +0700

    feat(action): ✨ create new task action for store data to task table

---
+++ src/action/task.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { TaskFormValues } from "@/components/custom/task/create-task-dialog";
+import { db } from "@/lib/db/prisma";
+import { taskFormSchema } from "@/schema";
+import { userRequired } from "@/utils/auth/user/user-auth";
+
+export const createNewTask = async (
+  data: TaskFormValues,
+  projectId: string,
+  workspaceId: string
+) => {
+  const { user } = await userRequired();
+  const validatedData = taskFormSchema.parse(data);
+  const isUserMember = await db.workspaceMember.findUnique({
+    where: {
+      userId_workspaceId: {
+        userId: user.id,
+        workspaceId,
+      },
+    },
+  });
+
+  if (!isUserMember) {
+    throw new Error("You are not a member of this workspace");
+  }
+
+  const tasks = await db.task.findMany({
+    where: {projectId},
+  });
+
+  const lastTask = tasks
+    ?.filter((task) => task.status === data.status)
+    .sort((a, b) => b.position - a.position)[0];
+
+    const position = lastTask ? lastTask.position + 1000 : 1000;
+
+  
+
+  const task = await db.task.create({
+    data: {
+      title: validatedData.title,
+      description: validatedData.description,
+      startDate: new Date(validatedData.startDate),
+      dueDate: new Date(validatedData.dueDate),
+      projectId,
+      assigneeId: validatedData.assigneeId,
+      status: validatedData.status,
+      priority: validatedData.priority,
+      position,
+    },
+    include: {
+      project: true,
+    },
+  });
+
+  await db.activity.create({
+    data: {
+      type: "TASK_CREATED",
+      description: `Task created: ${validatedData.title}`,
+      projectId,
+      userId: user.id,
+    }
+  });
+
+  return {success: true};
+};
