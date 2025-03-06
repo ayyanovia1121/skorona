76e81ff32404b05744f04f7edc8b8a2d240b2ba1
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-06 15:54:25 +0700

    feat(project): ✨ add task distribution chart component to project dashboard

--- src/components/custom/projects/project-dashboard.tsx
+++ src/components/custom/projects/project-dashboard.tsx
@@ -2,6 +2,8 @@ import { CommentProps, ProjectProps } from "@/types";
 import { Activity, Task } from "@prisma/client";
 import React from "react";
 import ProjectHeader from "./project-header";
+import { Card } from "@/components/ui/card";
+import TaskDistributionChart from "./task-distribution-chart";
 
 interface ProjectDashboardProps {
   project: ProjectProps;
@@ -23,13 +25,28 @@ const ProjectDashboard = ({
   totalWorkspaceMembers,
   comments,
 }: ProjectDashboardProps) => {
-  return <>
-  <div className="flex flex-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
-    <ProjectHeader 
-      project={project as unknown as ProjectProps}
-    />
-  </div>
-  </>;
+  return (
+    <>
+      <div className="flex flex-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
+        <ProjectHeader project={project as unknown as ProjectProps} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {/* Progress Circle */}
+          <Card></Card>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <TaskDistributionChart tasks={tasks} />
+          <Card>
+            <h3 className="text-lg font-semibold mb-4">Recent Activity</h3>
+            {/* <ActivityFeed activities={activities} /> */}
+          </Card>
+          <Card>
+            <h3 className="text-lg font-semibold mb-4">Recent Comments</h3>
+            {/* <CommentList comments={comments} /> */}
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectDashboard;
