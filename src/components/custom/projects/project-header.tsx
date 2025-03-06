944578f5fb060b9118ad7270e250a11ed2ca1050
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-06 11:44:03 +0700

    feat(project): ✨ add member card on project header

--- src/components/custom/projects/project-header.tsx
+++ src/components/custom/projects/project-header.tsx
@@ -1,9 +1,10 @@
-import { ProjectProps } from '@/types'
-import ProjectAvatar from './project-avatar'
-import CreateTaskDialog from '../task/create-task-dialog';
+import { ProjectProps } from "@/types";
+import ProjectAvatar from "./project-avatar";
+import CreateTaskDialog from "../task/create-task-dialog";
+import { Card } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-
-const ProjectHeader = ({project}:{project:ProjectProps}) => {
+const ProjectHeader = ({ project }: { project: ProjectProps }) => {
   return (
     <div className="space-y-4">
       <div className="flex flex-col md:flex-row md:items-center justify-between gap-2">
@@ -21,12 +22,31 @@ const ProjectHeader = ({project}:{project:ProjectProps}) => {
           </div>
         </div>
 
-        <div className='flex justify-end mt-3 md:mt-0 gap-3'>
-            <CreateTaskDialog project={project}/>
+        <div className="flex justify-end mt-3 md:mt-0 gap-3">
+          <CreateTaskDialog project={project} />
         </div>
       </div>
+
+      <Card className="p-4">
+        <div className="flex flex-col md:flex-row md:items-center gap-4">
+          <h3 className="text-sm font-medium">Team Members</h3>
+          <div className="flex flex-wrap space-x-2">
+            {project?.members?.map((member) => (
+              <Avatar
+                key={member.id}
+                className="size-9 2xl:size-10 border-2 border-background shadow"
+              >
+                <AvatarImage src={member?.user.image || undefined} />
+                <AvatarFallback className="text-sm 2xl:text-base">
+                  {member?.user.name.substring(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            ))}
+          </div>
+        </div>
+      </Card>
     </div>
   );
-}
+};
 
-export default ProjectHeader
+export default ProjectHeader;
