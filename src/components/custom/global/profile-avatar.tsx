baeaa71ab5e2922b0d109d2d9bce336dbeaff1c7
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-07 10:44:24 +0700

    feat(ui): ✨ create profile avatar component that can use globaly

---
+++ src/components/custom/global/profile-avatar.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+const ProfileAvatar = ({
+    url,
+    name,
+    size = "md",
+    className,
+    numOfChars = 1
+}: {
+    name: string;
+    url?: string;
+    size?: "sm" | "md" | "lg";
+    className?: string;
+    numOfChars?: number;
+}) => {
+  return (
+    <Avatar className={cn(
+        "h-8 w-8 rounded-md",
+        size === "sm" && "h-6 w-6",
+        size === "md" && "h-8 w-8",
+        size === "lg" && "h-10 w-10",
+        className
+    )}>
+        <AvatarImage src={url} alt={name} />
+        <AvatarFallback>
+            {name.substring(0, numOfChars).toUpperCase()}
+        </AvatarFallback>
+    </Avatar>
+  )
+}
+
+export default ProfileAvatar
