9a3728670e4c4a4bb82dffb8fbf17ba2420560b6
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-07 11:01:04 +0700

    feat(activity): ✅ done on activity feed

--- src/components/custom/projects/activity-feed.tsx
+++ src/components/custom/projects/activity-feed.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfileAvatar from "../global/profile-avatar";
+import { formatDistanceToNow } from "date-fns";
 
 export interface Activity {
   id: string;
@@ -21,12 +22,24 @@ const ActivityFeed = ({ activities }: ActivityFeedProps) => {
     <div className="space-y-4">
       {activities.map((activity) => (
         <div key={activity.id} className="flex items-start gap-4">
-          <ProfileAvatar 
-          name={activity.user.name} 
-          url={activity.user.image || undefined} 
-          numOfChars={2}
-          size="lg"
+          <ProfileAvatar
+            name={activity.user.name}
+            url={activity.user.image || undefined}
+            numOfChars={2}
+            size="lg"
           />
+
+          <div className="flex flex-col">
+            <p className="text-sm">
+              <span className="font-medium">{activity?.user?.name}</span>{" "}
+              {activity?.description}
+            </p>
+            <span className="text-xs text-muted-foreground">
+              {formatDistanceToNow(new Date(activity.createdAt), {
+                addSuffix: true,
+              })}
+            </span>
+          </div>
         </div>
       ))}
     </div>
