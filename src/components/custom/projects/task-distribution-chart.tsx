690568d2e8969aa9b4b8d7781acee70edcf3be7d
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-07 10:22:07 +0700

    feat(task): ✅ task distribution chart component done

--- src/components/custom/projects/task-distribution-chart.tsx
+++ src/components/custom/projects/task-distribution-chart.tsx
@@ -1,14 +1,21 @@
-'use client'
+"use client";
 
-import { ChartConfig } from "@/components/ui/chart";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Label, Pie, PieChart } from "recharts";
 
 interface TaskDistributionProps {
-    tasks: {
-        total: number;
-        completed: number;
-        inProgress: number;
-        overDue: number;
-    };
+  tasks: {
+    total: number;
+    completed: number;
+    inProgress: number;
+    overDue: number;
+  };
 }
 
 const chartConfig = {
@@ -31,28 +38,91 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 const TaskDistributionChart = ({ tasks }: TaskDistributionProps) => {
- const data = [
-  {
-    name: "Completed",
-    value: tasks.completed,
-    fill: "#22c55e",
-  },
-  {
-    name: "In Progress",
-    value: tasks.inProgress,
-    fill: "#3b82f6",
-  },
-  {
-    name: "Overdue",
-    value: tasks.overDue,
-    fill: "#ef4444",
-  },
-  {
-    name: "Todo",
-    value: tasks.total - tasks.completed - tasks.inProgress - tasks.overDue,
-    fill: "#f59e0b",
-  },
- ]
-}
+  const data = [
+    {
+      name: "Completed",
+      value: tasks.completed,
+      fill: "#22c55e",
+    },
+    {
+      name: "In Progress",
+      value: tasks.inProgress,
+      fill: "#3b82f6",
+    },
+    {
+      name: "Overdue",
+      value: tasks.overDue,
+      fill: "#ef4444",
+    },
+    {
+      name: "Todo",
+      value: tasks.total - (tasks.completed + tasks.inProgress + tasks.overDue),
+      fill: "#f59e0b",
+    },
+  ].filter((item) => item.value > 0);
 
-export default TaskDistributionChart
+  return (
+    <Card className="flex flex-col">
+      <CardHeader>
+        <CardTitle>Task Distribution</CardTitle>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={data}
+              dataKey={"value"}
+              nameKey={"name"}
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="central"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground font-bold text-3xl"
+                        >
+                          {tasks.total.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Tasks
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="text-center text-sm text-muted-foreground">
+        <div className="flex items-center justify-center w-full">
+          <p>Showing total task count for the project</p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default TaskDistributionChart;
