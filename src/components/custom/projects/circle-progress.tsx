e618dabe9dac50b5d52586bdc75c12b296ef130f
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-09 14:16:43 +0700

    style(ui): 🎨 update div tag style

--- src/components/custom/projects/circle-progress.tsx
+++ src/components/custom/projects/circle-progress.tsx
@@ -18,11 +18,11 @@ const variantStyle = {
 
 const CircleProgress = ({ title, value, subtitle, variant }: CircleProps) => {
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="flex flex-col items-center p-0">
       <h3 className="text-sm font-medium text-muted-foreground mb-2">
         {title}
       </h3>
-      <div className="relative w-20 h-20">
+      <div className="relative w-24 h-28">
         <Progress
           value={value}
           className={cn(
@@ -31,7 +31,9 @@ const CircleProgress = ({ title, value, subtitle, variant }: CircleProps) => {
           )}
         />
         <div className="absolute inset-0 flex items-center justify-center">
-          <span>{`${Math.round(value || 0)}%`}</span>
+          <span className={cn(`text-xl font-semibold ${variantStyle[variant || "default"] }`)}>{`${Math.round(
+            value || 0
+          )}%`}</span>
         </div>
         <p className="text-sm text-muted-foreground mt-2">{subtitle}</p>
       </div>
