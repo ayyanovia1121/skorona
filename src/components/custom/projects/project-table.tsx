4810aa5613f8216e9c59ea33586bdce8820d3ce7
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-09 15:13:16 +0700

    feat(project): ✨ create project table component that contained Datatable component

---
+++ src/components/custom/projects/project-table.tsx
@@ -0,0 +1,18 @@
+import { File, Project, Task, User } from '@prisma/client'
+import React from 'react'
+import { DataTable } from '../datatable/data-table';
+import { columns } from './column';
+
+interface TaskProps extends Task {
+    assignedTo: User;
+    project: Project;
+    attachments: File[];
+}
+
+const ProjectTable = ({tasks} : {tasks: TaskProps[]}) => {
+  return (
+    <DataTable columns={columns} data={tasks as any} />
+  )
+}
+
+export default ProjectTable
