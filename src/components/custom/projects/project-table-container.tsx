8e76874f9a02b3e7b45119f6ac8667aff1fa02b3
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-09 15:14:17 +0700

    feat(project): ✨ create project table component

---
+++ src/components/custom/projects/project-table-container.tsx
@@ -0,0 +1,15 @@
+import { getProjectById } from "@/utils/auth/project/get-project-by-id";
+import React from "react";
+import ProjectTable from "./project-table";
+
+const ProjectTableContainer = async ({ projectId }: { projectId: string }) => {
+  const { tasks } = await getProjectById(projectId);
+
+  return (
+    <div className="p-4">
+      <ProjectTable tasks={tasks as any} />
+    </div>
+  );
+};
+
+export default ProjectTableContainer;
