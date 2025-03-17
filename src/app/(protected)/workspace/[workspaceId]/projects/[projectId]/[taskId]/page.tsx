90b5ce41bd406727dc6715805cd0eb6bd00be19a
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-17 16:21:21 +0700

    feat(task): ✨ add interface props and render Task detail component

--- src/app/(protected)/workspace/[workspaceId]/projects/[projectId]/[taskId]/page.tsx
+++ src/app/(protected)/workspace/[workspaceId]/projects/[projectId]/[taskId]/page.tsx
@@ -1,10 +1,34 @@
+import TaskDetails from '@/components/custom/task/task-details'
+import { userRequired } from '@/utils/auth/user/user-auth'
+import { getTaskById } from '@/utils/data/task/get-task-by-id'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-type Props = {}
+interface PageProps {
+    params: Promise<{
+        taskId: string,
+        workspaceId: string,
+        projectId: string
+    }>
+}
 
-const TaskDetailPage = (props: Props) => {
+const TaskDetailPage = async({params}: PageProps) => {
+    await userRequired();
+
+    const {taskId, workspaceId, projectId} = await params;
+    const {task, comments} = await getTaskById(taskId, workspaceId, projectId);
+
+    if(!task) redirect("/not-found");
+
   return (
-    <div>TaskDetailPage</div>
+    <div className='flex flex-col lg:flex-row gap-6 md:px-6 pb-6'>
+        <div className="flex-1">
+            <TaskDetails task={task} />
+        </div>
+        <div className="w-full lg:w-[400px]">
+
+        </div>
+    </div>
   )
 }
 
