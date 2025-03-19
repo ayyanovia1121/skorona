f2e75d944feec8b169a01cca11d7057e1dc78e46
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-19 11:45:08 +0700

    feat(table): ✨ add task table columns with sorting and actions

--- src/components/custom/projects/column.tsx
+++ src/components/custom/projects/column.tsx
@@ -171,12 +171,174 @@ export const columns: ColumnDef<TaskTableItem>[] = [
                   View Task
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                Delete Task
-              </DropdownMenuItem>
+              <DropdownMenuItem asChild>Delete Task</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
+      );
+    },
+  },
+];
+
+export const myTaskColumns: ColumnDef<TaskTableItem>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Task Title <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const title = row.getValue("title") as string;
+
+      return (
+        <Link
+          href={`/workspace/${row.original.project.workspaceId}/projects/${row.original.project.id}/${row.original.id}`}
+        >
+          <div className="flex items-center gap-2">
+            <ProjectAvatar name={title} />
+            <span className="font-medium text-sm xl:text-base capitalize">
+              {title}
+            </span>
+          </div>
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <Button
+        variant={"ghost"}
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Status <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+
+      return (
+        <Badge variant={status as any}>
+          {status === "IN_PROGRESS" ? "In Progress" : status}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "priority",
+    header: "Priority",
+    cell: ({ row }) => {
+      const priority = row.getValue("priority") as string;
+
+      return (
+        <Badge variant={"secondary"} className="font-normal">
+          {priority}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ row }) => {
+      const createdAt = row.getValue("createdAt") as Date;
+      return <div>{format(new Date(createdAt), "dd/MM/yyyy")}</div>;
+    },
+  },
+  {
+    accessorKey: "dueDate",
+    header: "Due Date",
+    cell: ({ row }) => {
+      const dueDate = row.getValue("dueDate") as Date;
+      return <div>{format(new Date(dueDate), "dd/MM/yyyy")}</div>;
+    },
+  },
+  {
+    accessorKey: "project",
+    header: "Project",
+    cell: ({ row }) => {
+      const project = row.getValue("project") as {
+        name: string;
+      };
+      return (
+        <Link
+          href={`/workspace/${row.original.project.workspaceId}/projects/${row.original.project.id}`}
+        >
+          <div className="flex items-center gap-2">
+            <ProjectAvatar name={project?.name} />
+            <span className="font-medium text-sm xl:text-base capitalize">
+              {project?.name}
+            </span>
+          </div>
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "attachments",
+    header: "Attachments",
+    cell: ({ row }) => {
+      const attachments = row.getValue("attachments") as string[];
+
+      return (
+        <div className="flex items-center gap-2">
+          <Paperclip className="w-4 h-4" />
+          {attachments?.length}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size={"icon"}>
+              <EllipsisVertical className="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link
+                href={`/workspace/${row.original.project.workspaceId}/projects/${row.original.project.id}/${row.original.id}`}
+              >
+                View Task
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              Delete
+              {/* <DeleteTask taskId={row.original.id} /> */}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     },
   },
