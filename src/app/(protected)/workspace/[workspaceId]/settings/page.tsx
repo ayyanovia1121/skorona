3aec70bf588d0ec8b6c68bea3f5657bc645e296a
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-19 12:09:26 +0700

    feat(workspace): ✨ add workspaceSetting page

---
+++ src/app/(protected)/workspace/[workspaceId]/settings/page.tsx
@@ -0,0 +1,15 @@
+import { getWorkspaceProjectsById } from "@/utils/auth/project/get-workspace-project";
+
+type Props = {};
+
+const WorkspaceSettingsPage = async ({
+  params,
+}: {
+  params: Promise<{ workspaceId: string }>;
+}) => {
+  
+
+  return <div>WorkspaceSettingsPage</div>;
+};
+
+export default WorkspaceSettingsPage;
